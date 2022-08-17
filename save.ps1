ccaa013698fec668d602398182df464b747a5bcb
JamesSeo <roles3203@naver.com>
2022-08-17 17:06:48 +0900

    K8s practice on 2022-08-17 17:06:48

--- save.ps1
+++ save.ps1
@@ -6,5 +6,5 @@ git config --global user.email "roles3203@naver.com"
 git config --global user.name "JamesSeo"
 
 git add .
-git commit -m "K8s practice"
+git commit -m "K8s practice on `date +'%Y-%m-%d %H:%M:%S'`";
 git push -u origin master
