d9f5f1f36c2bc2d70b60d9495f4b5952b5d5b7ca
JamesSeo <roles3203@naver.com>
2022-08-17 17:00:37 +0900

    K8s practice

---
+++ save.ps1
@@ -0,0 +1,10 @@
+git init .
+
+git remote add origin https://github.com/SW4GGER23/Kubernetes.git
+
+git config --global user.email "roles3203@naver.com"
+git config --global user.name "JamesSeo"
+
+git add .
+git commit -m "K8s practice"
+git push -u origin master
